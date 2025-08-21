f4341b88c163c4f9d19d32f62d65d681feb8b3a0
Vigo <e1375607@u.nus.edu>
2025-08-21 16:53:23 +0800

    Implement 'border' variable

--- src/main/java/Falco.java
+++ src/main/java/Falco.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Falco {
     public static void main(String[] args) {
-        System.out.println("_________________________________");
+        String border = "_________________________________";
+        System.out.println(border);
         System.out.println("Hello! I'm Falco");
         System.out.println("What can I do for you?");
         System.out.println("_________________________________");
