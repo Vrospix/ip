7c5d22638dd2955d6f31dc13aecba6bf58b3ff6c
Vigo <e1375607@u.nus.edu>
2025-08-21 16:53:56 +0800

    Fix bug in echo

--- src/main/java/Falco.java
+++ src/main/java/Falco.java
@@ -6,7 +6,17 @@ public class Falco {
         System.out.println(border);
         System.out.println("Hello! I'm Falco");
         System.out.println("What can I do for you?");
-        System.out.println("_________________________________");
+        System.out.println(border);
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(border);
+            System.out.println(input + "? Yessir!");
+            System.out.println(border);
+
+            input = sc.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("_________________________________");
     }
