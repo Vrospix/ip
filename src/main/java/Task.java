bd1c3dd303bdf51b96a05cea4f81e4ac6f17b1ad
Vigo <e1375607@u.nus.edu>
2025-08-21 21:54:05 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private boolean done;
+    private final String task;
+
+    public Task(String task) {
+        this.done = false;
+        this.task = task;
+    }
+
+    public void mark() {
+        this.done = true;
+    }
+
+    public void unmark() {
+        this.done = false;
+    }
+
+    public String toString() {
+        if (this.done) {
+            return "[X] " + this.task;
+        } else {
+            return "[ ] " + this.task;
+        }
+    }
+}
