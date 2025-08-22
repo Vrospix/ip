0e347186183fceafe32d52d0c5ad4202daf63987
Vigo <e1375607@u.nus.edu>
2025-08-22 17:19:13 +0800

    Add new exception class for Falco

--- src/main/java/FalcoException.java
+++ src/main/java/FalcoException.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+public class FalcoException extends Exception{
+    public enum ErrorType {
+        UNKNOWN_COMMAND,
+        EMPTY_TASK,
+        NOTIME_DEADLINE,
+        UNCLEAR_EVENT
+    }
 
-public class FalcoException {
+    private ErrorType type;
+
+    public FalcoException(ErrorType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String getMessage() {
+        switch(type) {
+            case EMPTY_TASK:
+                return "Forgive me Sir, but you didn't specify the task... (￣^￣ )ゞ";
+            case UNKNOWN_COMMAND:
+                return "Negative Sir! Your command can't be understand ૮(˶ㅠ︿ㅠ)ა";
+            case NOTIME_DEADLINE:
+                return "Forgive me Sir, but you didn't correctly specify the time... ૮(˶ㅠ︿ㅠ)ა" +
+                        "\nYou should type it as (Deadline <task> /by <time>)";
+            case UNCLEAR_EVENT:
+                return "Forgive me Sir, but your event time is unclear... ૮(˶ㅠ︿ㅠ)ა" +
+                        "\nYou should type it as (Event <task> /from <start-time> /to <end-time>)";
+            default:
+                return "I don't understand, Sir ૮(˶ㅠ︿ㅠ)ა";
+        }
+    }
 }
