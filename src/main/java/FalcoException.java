c5b5e8c84dfa540df553ac9a8abad015865d2761
Vigo <e1375607@u.nus.edu>
2025-08-22 17:45:16 +0800

    Optimize exception handling

--- src/main/java/FalcoException.java
+++ src/main/java/FalcoException.java
@@ -3,7 +3,10 @@ public class FalcoException extends Exception{
         UNKNOWN_COMMAND,
         EMPTY_TASK,
         NOTIME_DEADLINE,
-        UNCLEAR_EVENT
+        UNCLEAR_EVENT,
+        UNCLEAR_DELETE,
+        UNCLEAR_MARK,
+        OUTOFBOUNDS
     }
 
     private ErrorType type;
@@ -21,10 +24,19 @@ public class FalcoException extends Exception{
                 return "Negative Sir! Your command can't be understand ૮(˶ㅠ︿ㅠ)ა";
             case NOTIME_DEADLINE:
                 return "Forgive me Sir, but you didn't correctly specify the time... ૮(˶ㅠ︿ㅠ)ა" +
-                        "\nYou should type it as (Deadline <task> /by <time>)";
+                        "\nYou should type it as: Deadline <task> /by <time>";
             case UNCLEAR_EVENT:
                 return "Forgive me Sir, but your event time is unclear... ૮(˶ㅠ︿ㅠ)ა" +
-                        "\nYou should type it as (Event <task> /from <start-time> /to <end-time>)";
+                        "\nYou should type it as: Event <task> /from <start-time> /to <end-time>";
+            case UNCLEAR_DELETE:
+                return "Uh Sir, which task specifically do you want to delete (ಠ_ಠ)?" +
+                        "\nSpecify it as: Delete <task-number>";
+            case UNCLEAR_MARK:
+                return "Uh Sir, which one are you referring to (ಠ_ಠ)?" +
+                        "\nSpecify it as: Mark/Unmark <task-number>";
+            case OUTOFBOUNDS:
+                return "Sir, that number is invalid. (ಠ_ಠ)" +
+                        "\nTry checking the task-list by typing: List";
             default:
                 return "I don't understand, Sir ૮(˶ㅠ︿ㅠ)ა";
         }
