d1556914f862dba7367fd0e82b808ae7457ddb75
Vigo <e1375607@u.nus.edu>
2025-08-22 17:17:29 +0800

    Add exception handling to Falco + Optimization

--- src/main/java/Falco.java
+++ src/main/java/Falco.java
@@ -3,55 +3,67 @@ import java.util.ArrayList;
 
 public class Falco {
     private ArrayList<Task> list = new ArrayList<>();
-    private final String border = "\t__________________________________________________________________";
+    private final String border = "_________________________________________________________" +
+            "_______________________________________________";
 
+    /** Put border before and after text **/
+    public void bordify(String message) {
+        String result = border + "\n" + message + "\n" + border;
+        System.out.println(result.indent(4));
+    }
+
+    /** Insert a task inside the list **/
     public void insertList(Task task) {
         list.add(task);
 
-        System.out.println(border);
-        System.out.println("\tYessir! I've added this task: ");
-        System.out.println("\t\t" + task);
-        System.out.println("\tNow you have " + list.size() + " tasks in the list, Sir! (￣^￣ )ゞ");
-        System.out.println(border);
+        String message = "Yessir! I've added this task: " +
+                        "\n\t" + task +
+                        "\nNow you have " + list.size() + " tasks in the list, Sir! (￣^￣ )ゞ";
+        bordify(message);
     }
 
+    /** Get the specific task from list **/
     public Task getTask(int i) {
         return list.get(i);
     }
 
+    /** Mark or Unmark the specific task in list **/
     public void markList(String action, int i) {
         if (action.equals("mark")) {
             list.get(i).mark();
-            System.out.println(border);
-            System.out.println("\tYessir! (￣^￣ )ゞ I've marked this task as done: ");
-            System.out.println("\t\t" + getTask(i));
-            System.out.println(border);
+            String message = "Yessir! (￣^￣ )ゞ I've marked this task as done: " +
+                            "\n\t" + getTask(i);
+            bordify(message);
         } else {
             list.get(i).unmark();
-            System.out.println(border);
-            System.out.println("\tAffirmative! (￣^￣ )ゞ I've marked this task as not done: ");
-            System.out.println("\t\t" + getTask(i));
-            System.out.println(border);
+            String message = "Affirmative! (￣^￣ )ゞ I've marked this task as not done: " +
+                            "\n\t" + getTask(i);
+            bordify(message);
         }
     }
 
+    /** Print out all the task inside list **/
     public void printList() {
         int n = list.size();
-        System.out.println(border);
-        System.out.println("\tSir, here are the tasks in your list: (￣^￣ )ゞ");
-        for(int i = 0; i < n; i++) {
-            System.out.println("\t" + (i + 1) + "." + list.get(i).toString());
+        if (n == 0) {
+            bordify("Sir, you haven't added any task yet (ಠ_ಠ)");
+        } else {
+            StringBuilder message = new StringBuilder("Sir, here are the tasks in your list: (￣^￣ )ゞ");
+            for (int i = 0; i < n; i++) {
+                message.append("\n" + (i + 1) + "." + list.get(i).toString());
+            }
+            bordify(message.toString());
         }
-        System.out.println(border);
     }
 
+    /** Greeting command **/
     public void greetings() {
-        System.out.println(border);
-        System.out.println("\tHello Sir! I'm Falco (￣^￣ )ゞ");
-        System.out.println("\tIt's an honor to be here");
-        System.out.println("\tWhat can I do for you?");
-        System.out.println(border);
+        String message = "Hello Sir! I'm Falco (￣^￣ )ゞ " +
+                        "\nIt's an honor to be here" +
+                        "\nWhat can I do for you?";
+        bordify(message);
     }
+
     public static void main(String[] args) {
         Falco falco = new Falco();
         falco.greetings();
@@ -59,58 +71,63 @@ public class Falco {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while(!input.equals("bye")) {
-            if (input.equals("list")) {
+            try {
+                if (input.toLowerCase().startsWith("list")) {
+                    falco.printList();
 
-                System.out.println(falco.border);
-                falco.printList();
-                System.out.println(falco.border);
+                } else if (input.toLowerCase().startsWith("mark") || input.toLowerCase().startsWith("unmark")) {
+                    String[] parts = input.split(" ");
+                    int index = Integer.parseInt(parts[1]) - 1;
+                    falco.markList(parts[0], index);
 
-            } else if (input.startsWith("mark") || input.startsWith("unmark")) {
-                String[] parts = input.split(" ");
-                int index = Integer.parseInt(parts[1]) - 1;
-                falco.markList(parts[0], index);
+                } else if (input.toLowerCase().startsWith("deadline")) {
+                    String[] parts = input.split(" ", 2);
+                    if (parts.length == 1) { throw new FalcoException(FalcoException.ErrorType.EMPTY_TASK); }
+                    String remaining = parts[1];
 
-            } else  if (input.startsWith("deadline")) {
-                String[] parts = input.split(" ", 2);
-                String remaining = parts[1];
+                    String[] details = remaining.split("/by", 2);
+                    if (details.length == 1) { throw new FalcoException(FalcoException.ErrorType.NOTIME_DEADLINE); }
+                    String desc = details[0].trim();
+                    String time = details[1].trim();
 
-                String[] details = remaining.split("/by", 2);
-                String desc = details[0].trim();
-                String time = details[1].trim();
+                    Task task = new Deadline(desc, time);
 
-                Task task = new Deadline(desc, time);
+                    falco.insertList(task);
+                } else if (input.toLowerCase().startsWith("event")) {
+                    String[] parts = input.split(" ", 2);
+                    if (parts.length == 1) { throw new FalcoException(FalcoException.ErrorType.EMPTY_TASK); }
+                    String remaining = parts[1];
 
-                falco.insertList(task);
-            } else if (input.startsWith("event")) {
-                String[] parts = input.split(" ", 2);
-                String remaining = parts[1];
+                    String[] details = remaining.split("/from", 2);
+                    if (details.length == 1) { throw new FalcoException(FalcoException.ErrorType.UNCLEAR_EVENT); }
+                    String desc = details[0].trim();
+                    String time = details[1];
 
-                String[] details = remaining.split("/from", 2);
-                String desc = details[0].trim();
-                String time = details[1];
+                    String[] spantime = time.split("/to", 2);
+                    if (spantime.length == 1) { throw new FalcoException(FalcoException.ErrorType.UNCLEAR_EVENT); }
+                    String from = spantime[0].trim();
+                    String to = spantime[1].trim();
 
-                String[] spantime = time.split("/to", 2);
-                String from = spantime[0].trim();
-                String to = spantime[1].trim();
+                    Task task = new Event(desc, from, to);
 
-                Task task = new Event(desc, from, to);
+                    falco.insertList(task);
+                } else if (input.toLowerCase().startsWith("todo")) {
+                    String[] parts = input.split(" ", 2);
+                    if (parts.length == 1) { throw new FalcoException(FalcoException.ErrorType.EMPTY_TASK); }
+                    String desc = parts[1].trim();
 
-                falco.insertList(task);
-            } else if (input.startsWith("todo")) {
-                String[] parts = input.split(" ", 2);
-                String desc = parts[1].trim();
+                    Task task = new Todo(desc);
 
-                Task task = new Todo(desc);
-
-                falco.insertList(task);
-            } else {
-                Task task = new Task(input);
-                falco.insertList(task);
+                    falco.insertList(task);
+                } else {
+                    throw new FalcoException(FalcoException.ErrorType.UNKNOWN_COMMAND);
+                }
+            } catch (FalcoException e) {
+                falco.bordify(e.getMessage());
             }
 
             input = sc.nextLine();
         }
-        System.out.println("\tBye Sir! It's an honor to work with you! (￣^￣ )ゞ");
-        System.out.println(falco.border);
+        falco.bordify("Bye Sir! It's an honor to work with you! (￣^￣ )ゞ");
     }
 }
